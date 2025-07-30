932651f6cd69f576d25990501ec98fc7e51c4675
Lori Framework Team <frameworklori@gmail.com>
2025-07-30 17:41:57 +0900

    Update auth.js

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,1 +1,16 @@
+const crypto = require('crypto-js');
 
+const verifyToken = (req, res, next) => {
+const token = req.headers['authorization'];
+if (!token) return res.status(403).json({ error: 'No token provided' });
+
+try {
+const decrypted = crypto.AES.decrypt(token, process.env.AES_KEY).toString(crypto.enc.Utf8);
+req.user = JSON.parse(decrypted);
+next();
+} catch (error) {
+res.status(401).json({ error: 'Invalid token' });
+}
+};
+
+module.exports = { verifyToken };
