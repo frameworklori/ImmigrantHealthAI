53f68b6a5a590ef6f796c664b1d8676bff6fb8df
Lori Framework Team <frameworklori@gmail.com>
2025-07-30 19:56:56 +0900

    Update test_crypto.js

--- tests/unit_tests/test_crypto.js
+++ tests/unit_tests/test_crypto.js
@@ -1,1 +1,22 @@
+const request = require('supertest');
+const app = require('../../src/api/server');
+
+describe('API Integration', () => {
+test('POST /api/allergy/detect', async () => {
+const res = await request(app)
+.post('/api/allergy/detect')
+.send({ image: "base64data" });
+expect(res.statusCode).toBe(200);
+expect(res.body).toHaveProperty('allergy');
+});
+
+test('POST /api/record/save', async () => {
+const res = await request(app)
+.post('/api/record/save')
+.set('Authorization', 'token')
+.send({ data: "health info" });
+expect(res.statusCode).toBe(200);
+expect(res.body).toHaveProperty('recordId');
+});
+});
 
