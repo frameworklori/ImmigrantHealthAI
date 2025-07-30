34498bff633763c1a7c3d21a1bdea4a83a8abc1f
Lori Framework Team <frameworklori@gmail.com>
2025-07-30 18:43:40 +0900

    Update Dashboard.js

--- frontend/pages/Dashboard.js
+++ frontend/pages/Dashboard.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import RecordForm from '../components/RecordForm';
+import AnonIDForm from '../components/AnonIDForm';
+
+const Dashboard = () => (
+<div>
+<h1>Dashboard</h1>
+<RecordForm />
+<AnonIDForm />
+</div>
+);
+
+export default Dashboard;
 
