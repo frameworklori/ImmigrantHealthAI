82e76ccfd42e5952d7eca1f83e7a97aa43d3fb90
Lori Framework Team <frameworklori@gmail.com>
2025-07-30 19:53:16 +0900

    Update crypto.js

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,1 +1,15 @@
+const CryptoJS = require('crypto-js');
+
+class Crypto {
+static encrypt(data) {
+return CryptoJS.AES.encrypt(data, process.env.AES_KEY).toString();
+}
+
+static decrypt(encrypted) {
+const bytes = CryptoJS.AES.decrypt(encrypted, process.env.AES_KEY);
+return bytes.toString(CryptoJS.enc.Utf8);
+}
+}
+
+module.exports = Crypto;
 
