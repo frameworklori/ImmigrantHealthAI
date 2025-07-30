e9d93ba12ea4096e8ecad7089295aa06882a7b0c
Lori Framework Team <frameworklori@gmail.com>
2025-07-30 18:42:50 +0900

    Update Home.js

--- frontend/pages/Home.js
+++ frontend/pages/Home.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import Camera from '../components/Camera';
 
+const Home = () => (
+<div>
+<h1>Welcome to ImmigrantHealthAI</h1>
+<p>Reduce healthcare costs ($0.2/use, $900M/year) for 11M immigrants.</p>
+<Camera />
+</div>
+);
+
+export default Home;
