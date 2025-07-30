79b8187760b1aa58cfbb0e8b3ff756fb78f5f192
Lori Framework Team <frameworklori@gmail.com>
2025-07-30 18:44:20 +0900

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 
+const App = () => (
+<Router>
+<Switch>
+<Route exact path="/" component={Home} />
+<Route path="/dashboard" component={Dashboard} />
+</Switch>
+</Router>
+);
+
+export default App;
