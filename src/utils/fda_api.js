5ede2c8ab7c992323ef994f301ea9dc2678a4c4c
Lori Framework Team <frameworklori@gmail.com>
2025-07-30 19:53:50 +0900

    Update fda_api.js

--- src/utils/fda_api.js
+++ src/utils/fda_api.js
@@ -1,1 +1,14 @@
+const axios = require('axios');
 
+class FDAAPI {
+static async fetchDrugInfo(drugName) {
+try {
+const response = await axios.get(`https://api.fda.gov/drug/label?api_key=${process.env.FDA_API_KEY}&search=${encodeURIComponent(drugName)}`);
+return response.data.results[0]?.openfda?.generic_name || 'unknown';
+} catch (error) {
+return 'error';
+}
+}
+}
+
+module.exports = FDAAPI;
